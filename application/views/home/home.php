c1130d91aa06732c2ce4afe2e09ac2b7f8618398
ZeroLie <fabianaakamine@hotmail.com>
2021-01-04 21:36:30 -0300

    Testes

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -33,7 +33,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-sm-12 co-xs-12">
-                    <h3 class="text-title">Com Altas Avaliações</h3>
+                    <h3 class="text-title">Com Altas Avaliações aaa</h3>
                 </div> 
                 <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 pb-4">
                     <div class="card h-100">
