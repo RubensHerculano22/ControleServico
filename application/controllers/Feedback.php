492a3ca273834247c60d5fcdd435e01867aac4ec
BuriedBullet <rubensherculano@hotmail.com>
2021-05-31 21:55:16 -0300

    Começo da parte de Feedback

---
+++ application/controllers/Feedback.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Feedback extends CI_Controller{
+
+    //listar_categorias muda no sistema_model
+    function __construct()
+    {
+        parent:: __construct();
+        $this->data = array();
+
+        $this->dados = $this->session->userdata("dados" . APPNAME);
+        $this->data["dados"] = $this->dados;
+        
+        $this->load->model("Feedback_model", "m_feedback");
+        $this->load->model("Servico_model", "m_servico");
+
+        $local = str_replace("TCC/", "", $_SERVER["REQUEST_URI"]);
+        $local = str_replace("tcc/", "", $local);
+        
+        $this->session->set_userdata(array("local" => $local));
+        $this->data["local"] = $local;
+        $this->data["cidade"] = $this->session->userdata("cidade");
+
+        $this->data["categorias"] = $this->m_sistema->listar_categorias();
+        $this->data["estados"] = $this->m_servico->get_estados();
+
+        $this->data["header"] = $this->load->view("template/header", $this->data, true);
+        $this->data["navbar"] = $this->load->view("template/navbar", $this->data, true);
+        $this->data["sidebar"] = $this->load->view("template/sidebar", $this->data, true);
+        $this->data["footer"] = $this->load->view("template/footer", $this->data, true);
+    }
+
+    public function index()
+    {
+        $this->data["breadcrumb"] = (object)array("titulo" => "Feedback", "before" => array((object) array("nome" => "Home", "link" => "Servico")), "current" => "Feedback de Serviço");
+
+        $this->data["content"] = $this->load->view("feedback/home", $this->data, true);
+        $this->load->view("template/content", $this->data);
+    }
+    
+}
---
+++ application/views/feedback/home.php
@@ -0,0 +1,24 @@
+<div class="container-fluid p-5">
+    <div class="row">
+        <div class="col-md-9 col-sm-9 col-xs-12">
+            <div class="card">
+                <div class="card-header" style="background-color: #e36802">
+                    <h3 class="card-title text-white">Feedback do Serviço</h3>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-12">
+            <div class="card">
+                <div class="card-header" style="background-color: #e36802">
+                    <h3 class="card-title text-white">Serviço</h3>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
