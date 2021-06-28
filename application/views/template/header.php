ec779faab0915d6a5d83256f3bec356565f5d5ed
BuriedBullet <rubensherculano@hotmail.com>
2021-06-27 21:29:01 -0300

    troca famicon

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="shortcut icon" href="<?= base_url("assets/img/favicon.png") ?>" />
+  <link rel="shortcut icon" href="<?= base_url("assets/img/logo.png") ?>" />
 
   <title>NextoYou</title>
 
